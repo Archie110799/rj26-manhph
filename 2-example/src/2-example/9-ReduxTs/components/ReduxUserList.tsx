0759c2cbd259646cb0c4514413b9fdeba6a501e1
manhph-macos <manhph@manhph-macoss-MacBook-Pro.local>
2022-11-23 19:00:24 +0700

    add handle delete

--- 2-example/src/2-example/9-ReduxTs/components/ReduxUserList.tsx
+++ 2-example/src/2-example/9-ReduxTs/components/ReduxUserList.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getUsersAsync } from "../actions/userAction";
+import { deleteUsersAsync, getUsersAsync } from "../actions/userAction";
 
 interface IState {
   userReducers: IUserReducer;
@@ -28,12 +28,30 @@ function ReduxUserList() {
     dispatch(getUsersAsync());
   }, []);
 
+  const handleDelete = (userId : string | number) =>{
+    console.log(userId);
+    dispatch(deleteUsersAsync(userId));
+  }
   return (
     <>
       {loading ? <p>Loadding...</p> : null}
-      {users?.map((user) => (
-        <p key={user.id}>{user.name}</p>
-      ))}
+      <div className="d-flex justify-content-around flex-wrap">
+        {users?.map((user) => (
+          <div key={user.id} className="card w-25 m-3">
+            <img src={user.avatar} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title text-truncate">{user.name}</h5>
+              <p className="card-text text-truncate">{user.email}</p>
+              <div className="d-flex justify-content-around">
+                <a href="#" className="btn btn-primary">
+                  More...
+                </a>
+                <span onClick={()=>handleDelete(user.id)} className="btn btn-danger">Delete</span>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </>
   );
 }
